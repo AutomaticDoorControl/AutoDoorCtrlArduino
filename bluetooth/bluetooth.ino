fc0b9062cbd7cda60db3311aa6e26a2489bc74d1
ljw980105 <ljw9801055@gmail.com>
2019-09-19 19:09:52 -0400

    Added Readme

--- bluetooth/bluetooth.ino
+++ bluetooth/bluetooth.ino
@@ -1,42 +1,42 @@
 #include <SoftwareSerial.h>
 #include "outgoingCommands.h"
 
-#define LED_PIN 5
+#define DOOR_PIN 5
 #define txPin 11
 #define rxPin 10
 
 SoftwareSerial adc(rxPin, txPin);
 
 
-# pragma mark - function prototypes
+#pragma mark - function prototypes
 
 void processData2();
 
-# pragma mark - main arduino methods
+#pragma mark - main arduino methods
 
 void setup() {
     pinMode(rxPin, INPUT);
     pinMode(txPin, OUTPUT);
-    pinMode(LED_PIN, OUTPUT);
+    pinMode(DOOR_PIN, OUTPUT);
     Serial.begin(9600);
     adc.begin(9600);
 }
 
 void loop() {
-    processData2();
+    processData();
 }
 
 # pragma mark - helper methods
 
-void processData2() {
+void processData() {
     if (adc.available()) {
         char data = adc.read();
         Serial.println(data);
         if (data == 'a') {
             adc.print(onCommand);
-            digitalWrite(LED_PIN, HIGH);
+            digitalWrite(DOOR_PIN, HIGH);
             delay(2000);
-            digitalWrite(LED_PIN, LOW);
+            digitalWrite(DOOR_PIN, LOW);
             delay(20000);
             adc.print(offCommand);
         }
